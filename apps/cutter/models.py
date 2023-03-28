f4bc33d404d28b1273fb58b7c68648ab359f9855
Vladislav Fadeev <a9144199959@mail.ru>
2023-03-28 23:24:44 +1000

    change cutter app db model

--- apps/cutter/models.py
+++ apps/cutter/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 from django.conf import settings
 from django.urls import reverse
 from django.db import models
@@ -7,12 +7,46 @@ import string
 import random
 
 
+User = get_user_model()
+
+
+class GroupLinkModel(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+    is_private = models.BooleanField(default=True)
+    password = models.CharField(max_length=128)
+
+
 class LinkModel(models.Model):
-    slug = models.SlugField(max_length=15, unique=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    slug = models.SlugField(
+        max_length=30,
+        unique=True
+    )
     long_link = models.URLField(max_length=30000)
     statistics = models.BooleanField(default=False)
-    
-
+    group = models.ForeignKey(
+        GroupLinkModel,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    is_private = models.BooleanField(default=False)
+    password = models.CharField(max_length=128)
+    svg_image = models.CharField(max_length=30)
+    png_image = models.CharField(max_length=30)
+    date_created = models.DateField(auto_now_add=True)
+    date_expire = models.DateField()
+    use_alias = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     def make_slug():
         count = 0
@@ -43,7 +77,10 @@ class LinkModel(models.Model):
 
 class LinkTransitionsModel(models.Model):
 
-    link = models.ForeignKey(LinkModel, on_delete=models.CASCADE)
+    link = models.ForeignKey(
+        LinkModel,
+        on_delete=models.CASCADE
+    )
     user_agent_unparsed = models.CharField(max_length=150)
     device = models.CharField(max_length=20)
     os = models.CharField(max_length=20)
