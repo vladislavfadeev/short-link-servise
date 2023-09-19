d3e712281670b9039edf40942a494b01ad35e36b
Vladislav Fadeev <a9144199959@mail.ru>
2023-09-19 19:23:12 +0800

    pre-done v8

--- clkr_core/settings/logging.py
+++ clkr_core/settings/logging.py
@@ -1,29 +1,35 @@
-import os
-
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {
-        "verbose": {
-            "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+        "basic": {
+            "format": "%(asctime)s - [%(levelname)s] - %(name)s "
+            "- (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s",
             "datefmt": "%d/%b/%Y %H:%M:%S",
         },
-        "simple": {"format": "%(levelname)s %(message)s"},
     },
     "handlers": {
-        # 'file': {
-        #     'level': 'ERROR',
-        #     'class': 'logging.FileHandler',
-        #     'filename': LOG_FILE,
-        #     'formatter': 'verbose'
-        # },
-        "console": {"class": "logging.StreamHandler", "formatter": "verbose"},
+        "console": {
+            "level": "WARNING",
+            "formatter": "basic",
+            "class": "logging.StreamHandler",
+            # 'stream': 'ext://sys.stdout',  # Default is stderr
+        },
+        "file": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "level": "WARNING",
+            "formatter": "basic",
+            "filename": "/src/logs/clkr-service.log",
+            "maxBytes": 1048576,
+            "backupCount": 10,
+            "mode": "a",
+        },
     },
     "loggers": {
         "django": {
-            "handlers": ["console"],
+            "handlers": ["console, file"],
             "propagate": True,
-            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
-        },
+            "level": "WARNING",
+        }
     },
 }
